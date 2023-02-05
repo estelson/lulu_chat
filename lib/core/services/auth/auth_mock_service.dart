a1d9f682397e164f3617f0bfeeb5f6b5b2d57ac8
Estelson Medeiros Pereira <estelson@gmail.com>
2023-02-05 12:20:57 -0300

    Criação de um usuário default para testes
    - Será modificado posteriormente quando será utilizado o Firebase Auth

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -6,13 +6,19 @@ import 'package:lulu_chat/core/models/chat_user.dart';
 import 'package:lulu_chat/core/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
-  static Map<String, ChatUser> _users = {};
+  static final _defaultUser = ChatUser(id: "1", name: "Teste", email: "teste@teste.com", imageUrl: "assets/images/avatar.png");
+
+  static Map<String, ChatUser> _users = {
+    _defaultUser.email: _defaultUser,
+  };
+
   static ChatUser? _currentUser;
+
   static MultiStreamController<ChatUser?>? _controller;
 
   static final _userStream = Stream<ChatUser?>.multi((controller) {
     _controller = controller;
-    _updateUser(null);
+    _updateUser(_defaultUser);
   });
 
   ChatUser? get getCurrentUser {
