c9ab9f32d196385949c69382d87c07d750f558b1
Estelson Medeiros Pereira <estelson@gmail.com>
2023-02-05 20:24:02 -0300

    Configurando o serviço de notificação #01

---
+++ lib/core/services/notification/push_notification_service.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:lulu_chat/core/models/chat_notification.dart';
+
+class PushNotificationService with ChangeNotifier {
+  List<ChatNotification> _items = [];
+
+  List<ChatNotification> get getItems {
+    return [..._items];
+  }
+
+  void add(ChatNotification notification) {
+    _items.add(notification);
+
+    notifyListeners();
+  }
+
+  void remove(int i) {
+    _items.removeAt(i);
+
+    notifyListeners();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lulu_chat/core/services/auth/auth_or_app_page.dart';
+import 'package:lulu_chat/core/services/notification/push_notification_service.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,26 +12,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          centerTitle: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => PushNotificationService(),
         ),
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: Colors.blue,
-          secondary: Colors.white,
-          error: Colors.red,
-        ),
-        // canvasColor: Color.fromRGBO(220, 234, 255, 1.0),
-        textTheme: ThemeData.light().textTheme.copyWith(
-              titleLarge: TextStyle(
-                fontSize: 20,
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            centerTitle: true,
+          ),
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.blue,
+            secondary: Colors.white,
+            error: Colors.red,
+          ),
+          // canvasColor: Color.fromRGBO(220, 234, 255, 1.0),
+          textTheme: ThemeData.light().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  fontSize: 20,
+                ),
               ),
-            ),
+        ),
+        home: AuthOrAppPage(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: AuthOrAppPage(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
