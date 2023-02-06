de456fec62218b7a9b5a7c8f21f40b7c3d84e9ad
Estelson Medeiros Pereira <estelson@gmail.com>
2023-02-05 23:46:18 -0300

    Autenticação Com Firebase #01

--- lib/core/services/auth/auth_service.dart
+++ lib/core/services/auth/auth_service.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:lulu_chat/core/models/chat_user.dart';
-import 'package:lulu_chat/core/services/auth/auth_mock_service.dart';
+import 'package:lulu_chat/core/services/auth/auth_firebase_service.dart';
 
 abstract class AuthService {
   ChatUser? get getCurrentUser;
@@ -15,7 +15,7 @@ abstract class AuthService {
   Future<void> logout();
 
   factory AuthService() {
-    return AuthMockService();
-    // return AuthFirebaseService();
+    // return AuthMockService();
+    return AuthFirebaseService();
   }
 }
