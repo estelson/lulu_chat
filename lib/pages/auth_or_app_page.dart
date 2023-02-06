1696b3d4284915d4bf1a376f8490a43fa8503104
Estelson Medeiros Pereira <estelson@gmail.com>
2023-02-05 23:24:08 -0300

    Iniciando o Firebase no app

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -4,22 +4,35 @@ import 'package:lulu_chat/core/services/auth/auth_service.dart';
 import 'package:lulu_chat/pages/chat_page.dart';
 import 'package:lulu_chat/pages/auth_page.dart';
 import 'package:lulu_chat/pages/loading_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({Key? key}) : super(key: key);
 
+  Future<void> init(BuildContext context) async {
+    await Firebase.initializeApp();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: StreamBuilder<ChatUser?>(
-      stream: AuthService().getUserChanges,
+    return FutureBuilder(
+      future: init(context),
       builder: (ctx, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return LoadingPage();
         } else {
-          return snapshot.hasData ? ChatPage() : AuthPage();
+          return StreamBuilder<ChatUser?>(
+            stream: AuthService().getUserChanges,
+            builder: (ctx, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return LoadingPage();
+              } else {
+                return snapshot.hasData ? ChatPage() : AuthPage();
+              }
+            },
+          );
         }
-      },
-    ));
+      }
+    );
   }
 }
