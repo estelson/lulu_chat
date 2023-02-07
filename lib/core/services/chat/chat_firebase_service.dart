f187df9c98ddaf5eada8338e4c9674051dccad4d
Estelson Medeiros Pereira <estelson@gmail.com>
2023-02-07 01:11:24 -0300

    Obtendo Mensagens do Firestore #01

--- lib/core/services/chat/chat_firebase_service.dart
+++ lib/core/services/chat/chat_firebase_service.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:lulu_chat/core/models/chat_message.dart';
@@ -7,7 +8,29 @@ import 'package:lulu_chat/core/services/chat/chat_service.dart';
 
 class ChatFirebaseService implements ChatService {
   Stream<List<ChatMessage>> messagesStream() {
-    return Stream<List<ChatMessage>>.empty();
+    final store = FirebaseFirestore.instance;
+
+    final snapshots = store.collection("chat").withConverter(
+          fromFirestore: _fromFirestore,
+          toFirestore: _toFirestore,
+        ).snapshots();
+
+    return snapshots.map((snapshot) {
+      return snapshot.docs.map((doc) {
+        return doc.data();
+      }).toList();
+    });
+
+    // --- Alternativa mais verbosa porém mais legível ---
+    // return Stream<List<ChatMessage>>.multi((controller) {
+    //   snapshots.listen((snapshot) {
+    //     List<ChatMessage> lista = snapshot.docs.map((doc) {
+    //       return doc.data();
+    //     }).toList();
+    //
+    //     controller.add(lista);
+    //   });
+    // });
   }
 
   Future<ChatMessage?> save(String text, ChatUser user) async {
