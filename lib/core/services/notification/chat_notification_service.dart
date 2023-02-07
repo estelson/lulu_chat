8ef67d10896695565e6279cad6143ad2cde71f28
Estelson Medeiros Pereira <estelson@gmail.com>
2023-02-07 11:57:26 -0300

    Push notification #01
    - Utiliza o Firebase Messaging para enviar notificações de push para o app

--- lib/core/services/notification/chat_notification_service.dart
+++ lib/core/services/notification/chat_notification_service.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:lulu_chat/core/models/chat_notification.dart';
 
@@ -22,5 +23,30 @@ class ChatNotificationService with ChangeNotifier {
     _items.removeAt(i);
 
     notifyListeners();
+  }
+
+  // Push notification
+  Future<void> init() async {
+    _configureForeground();
+  }
+
+  Future<bool> get _isAuthorized async {
+    final messaging = FirebaseMessaging.instance;
+    final settings = await messaging.requestPermission();
+
+    return settings.authorizationStatus == AuthorizationStatus.authorized;
+  }
+
+  Future<void> _configureForeground() async {
+    if (await _isAuthorized) {
+      FirebaseMessaging.onMessage.listen((msg) {
+        if(msg.notification == null) return;
+
+        add(ChatNotification(
+          title: msg.notification!.title! ?? "Não informado!",
+          body: msg.notification!.body! ?? "Não informado!",
+        ));
+      });
+    }
   }
 }
--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:lulu_chat/core/models/chat_user.dart';
 import 'package:lulu_chat/core/services/auth/auth_service.dart';
+import 'package:lulu_chat/core/services/notification/chat_notification_service.dart';
 import 'package:lulu_chat/pages/chat_page.dart';
 import 'package:lulu_chat/pages/auth_page.dart';
 import 'package:lulu_chat/pages/loading_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({Key? key}) : super(key: key);
 
   Future<void> init(BuildContext context) async {
     await Firebase.initializeApp();
+    await Provider.of<ChatNotificationService>(context, listen: false).init();
   }
 
   @override
